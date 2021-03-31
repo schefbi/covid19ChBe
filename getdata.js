c7987e695d5298cf23cfc6d9d450482576ec1476
SCHE <sandroscheidt@icloud.com>
2021-03-31 16:10:46 +0200

    new statistics vent 7 days average

--- getdata.js
+++ getdata.js
@@ -9,47 +9,54 @@ Papa.parse(covidFile, {
         
         var lastvalue = 1;
         var lastvalueVent = 0;
-        var newPositivPerDay = [];
+        var jsonData = [];
         results.data.forEach(item => {
             
             var vent =  Number(item.current_vent) === 0 || item.current_vent === '' ? lastvalueVent : Number(item.current_vent);
             var ncumul_conf = Number(item.ncumul_conf) === 0 ? lastvalue : Number(item.ncumul_conf);
             //console.log(item.date, ncumul_conf, Number(item.ncumul_conf));
             newItem = {date: item.date, newPositiv: ncumul_conf - lastvalue, vent: vent, deads: item.ncumul_deceased};
-            newPositivPerDay.push(newItem);
+            jsonData.push(newItem);
             
             lastvalue = ncumul_conf;
             lastvalueVent = vent;
         });
 
-        console.log(newPositivPerDay);
-        var sevenDayAverage = get7dayAverage(newPositivPerDay);
+        //console.log(jsonData);
+        var sevenDayAverage = get7dayAverage(jsonData,'newPositiv');
         //console.log(sevenDayAverage);
         var previousWeekInPercent = getPreviousWeekInPercent(sevenDayAverage);
+        var vent7DayAverage = get7dayAverage(jsonData,'vent');
         var statisticData = [];
         statisticData.push(sevenDayAverage);
         statisticData.push(previousWeekInPercent);
+        statisticData.push(vent7DayAverage);
           
-        //console.log(statisticData);
+        console.log(statisticData);
 	    sessionStorage.removeItem('data');
         sessionStorage.setItem('data',JSON.stringify(statisticData));
     }
 });
 
 
-function get7dayAverage(newPositivPerDay) {
+function get7dayAverage(jsonData, column) {
 
     var i = 0;
     var sum7Day = 0;
     var average;
     var result = [];
         
-    newPositivPerDay.forEach(item => {
+    jsonData.forEach(item => {
         
         var last7Day = getLast7Day(i);
         var last7DayRow = getLast7DayRow(i);
 
-        sum7Day =  sum7Day + item.newPositiv - newPositivPerDay[last7DayRow].newPositiv;
+        if(column === 'vent'){
+            sum7Day =  sum7Day + item.vent - jsonData[last7DayRow].vent;
+        } else {
+            sum7Day =  sum7Day + item.newPositiv - jsonData[last7DayRow].newPositiv;
+        }
+        
         average = last7Day < 0 ? 0 : sum7Day/7;
 
         result.push({date: item.date, value: average});
